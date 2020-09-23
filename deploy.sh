0aa1baca35c41f3d1bdea69ee429d587ad77071f
Chris Ashworth <chris@figure53.com>
2020-09-23 14:33:17 -0400

    Rename directory structure.

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rsync -avz --exclude '.git' --exclude '.gitignore'  --exclude-from '.gitignore' ./app/ deploy@madewithloveinbaltimore.org:/home/deploy/madewithloveinbaltimore.org
+rsync -avz --exclude '.git' --exclude '.gitignore'  --exclude-from '.gitignore' ./src/ deploy@madewithloveinbaltimore.org:/home/deploy/madewithloveinbaltimore.org
