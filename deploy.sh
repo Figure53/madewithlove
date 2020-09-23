078bffd3cd7afe36a3e0f57ace0c2d91056f33a0
Chris Ashworth <chris@figure53.com>
2020-09-23 13:34:06 -0400

    Organize repository, update method for deploying to a new server.

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-ssh figure53@figure53.com 'cd ~/webapps/htdocs_love; git pull origin master'
+
+rsync -avz --exclude '.git' --exclude '.gitignore'  --exclude-from '.gitignore' ./app/ deploy@madewithloveinbaltimore.org:/home/deploy/madewithloveinbaltimore.org
